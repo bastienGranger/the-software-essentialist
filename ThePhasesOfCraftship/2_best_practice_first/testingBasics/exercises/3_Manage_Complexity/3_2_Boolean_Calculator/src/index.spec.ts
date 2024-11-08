4bb3d3be1f0126178524e7575288a968430ffa1c
Bastien Granger <grangerbastien@gmail.com>
2024-11-08 09:13:08 -0500

    feat(booleanCalculator): able to calculate from 2 booleans value

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
@@ -1,20 +1,66 @@
 import { BooleanCalculator } from "./index";
 
 describe("boolean calculator", () => {
+  let booleanCalculator: BooleanCalculator;
+
+  beforeEach(() => {
+    booleanCalculator = new BooleanCalculator();
+  });
+
+  it("should throw an Invalid entry if entry is empty", () => {
+    expect(() => booleanCalculator.calculate("")).toThrowError("Invalid entry");
+  });
+
+  it("should throw an Invalid entry error if entry is not a boolean", () => {
+    expect(() => booleanCalculator.calculate("INVALID")).toThrowError(
+      "Invalid entry",
+    );
+  });
+
+  it("should throw an Invalid entry error if entry is an operator and not boolean", () => {
+    expect(() => booleanCalculator.calculate("AND")).toThrowError(
+      "Invalid entry",
+    );
+    expect(() => booleanCalculator.calculate("OR")).toThrowError(
+      "Invalid entry",
+    );
+  });
+
   it("should know that TRUE is true", () => {
-    const booleanCalculator = new BooleanCalculator();
     expect(booleanCalculator.calculate("TRUE")).toBe(true);
   });
 
   it("should know that FALSE is false", () => {
-    const booleanCalculator = new BooleanCalculator();
     expect(booleanCalculator.calculate("FALSE")).toBe(false);
   });
 
-  it("should throw an error if entry is not a boolean", () => {
-    const booleanCalculator = new BooleanCalculator();
-    expect(() => booleanCalculator.calculate("INVALID")).toThrowError(
-      "Invalid entry",
+  describe("given an entry with 2 booleans and the AND operator", () => {
+    it.each`
+      expression           | expected
+      ${"TRUE AND TRUE"}   | ${true}
+      ${"TRUE AND FALSE"}  | ${false}
+      ${"FALSE AND TRUE"}  | ${false}
+      ${"FALSE AND FALSE"} | ${false}
+    `(
+      "should know that $expression is $expected",
+      ({ expression, expected }) => {
+        expect(booleanCalculator.calculate(expression)).toBe(expected);
+      },
+    );
+  });
+
+  describe("given an entry with 2 booleans and the OR operator", () => {
+    it.each`
+      expression          | expected
+      ${"TRUE OR TRUE"}   | ${true}
+      ${"TRUE OR FALSE"}  | ${true}
+      ${"FALSE OR TRUE"}  | ${true}
+      ${"FALSE OR FALSE"} | ${false}
+    `(
+      "should know that $expression is $expected",
+      ({ expression, expected }) => {
+        expect(booleanCalculator.calculate(expression)).toBe(expected);
+      },
     );
   });
 });
--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.ts
@@ -1,8 +1,46 @@
 export class BooleanCalculator {
   public calculate(expression: string): boolean {
-    if (expression !== "TRUE" && expression !== "FALSE") {
+    const expressionParts = expression.split(" ").map((part) => part.trim());
+
+    if (expressionParts.length < 1 || expressionParts.length > 3) {
       throw new Error("Invalid entry");
     }
-    return expression === "TRUE";
+
+    if (expressionParts.length === 1) {
+      return this.getBooleanValue(expressionParts[0]);
+    }
+
+    if (expressionParts.length % 2 === 0) {
+      throw new Error("Invalid entry");
+    }
+
+    const first = this.getBooleanValue(expressionParts[0]);
+    const second = this.getBooleanValue(expressionParts[2]);
+
+    if (!this.isValidOperator(expressionParts[1])) {
+      throw new Error("Invalid entry");
+    }
+    return this.isAndOperator(expressionParts[1])
+      ? first && second
+      : first || second;
+  }
+
+  private getBooleanValue(str: string): boolean {
+    if (str === "TRUE" || str === "FALSE") {
+      return str === "TRUE";
+    }
+    throw new Error("Invalid entry");
+  }
+
+  private isAndOperator(str: string): boolean {
+    return str === "AND";
+  }
+
+  private isOrOperator(str: string): boolean {
+    return str === "OR";
+  }
+
+  private isValidOperator(str: string): boolean {
+    return this.isAndOperator(str) || this.isOrOperator(str);
   }
 }
