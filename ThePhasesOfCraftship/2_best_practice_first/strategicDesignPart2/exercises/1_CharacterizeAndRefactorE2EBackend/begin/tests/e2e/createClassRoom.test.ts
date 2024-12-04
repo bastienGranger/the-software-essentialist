16160d6283c988900a6e99f67e2abe50b42bfe90
Bastien Granger <grangerbastien@gmail.com>
2024-12-04 16:39:31 -0500

    test(createClassRoom): implement first test cases

--- ThePhasesOfCraftship/2_best_practice_first/strategicDesignPart2/exercises/1_CharacterizeAndRefactorE2EBackend/begin/tests/e2e/createClassRoom.test.ts
+++ ThePhasesOfCraftship/2_best_practice_first/strategicDesignPart2/exercises/1_CharacterizeAndRefactorE2EBackend/begin/tests/e2e/createClassRoom.test.ts
@@ -2,6 +2,7 @@ import { defineFeature, loadFeature } from "jest-cucumber";
 import request from "supertest";
 import path from "path";
 import { app } from "../../src/index";
+import { resetDatabase } from "../fixtures/reset";
 
 const feature = loadFeature(
   path.join(__dirname, "../features/createClassRoom.feature"),
@@ -24,15 +25,26 @@ defineFeature(feature, (test) => {
 
     then("the class room should be created successfully", () => {
       expect(response.status).toBe(201);
+      expect(response.body.success).toBeTruthy();
       expect(response.body.data.name).toBe(requestBody.name);
     });
   });
 
   test("Fail to create a class room", ({ given, when, then }) => {
-    given("I want to create a class room with no name", () => {});
+    let requestBody: any = {};
+    let response: any = {};
+    given("I want to create a class room with no name", () => {
+      requestBody = {};
+    });
 
-    when("I send a request to create a class room", () => {});
+    when("I send a request to create a class room", async () => {
+      response = await request(app).post("/classes").send(requestBody);
+    });
 
-    then("the class room should not be created", () => {});
+    then("the class room should not be created", () => {
+      expect(response.status).toBe(400);
+      expect(response.body.success).toBeFalsy();
+      expect(response.body.error).toBe("ValidationError");
+    });
   });
 });
